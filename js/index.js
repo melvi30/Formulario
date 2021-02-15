e4ae6db66eeec70e1f7cef2309d14dc8f4b68728
melvi30 <marianamujica30@gmail.com>
2021-02-15 13:37:07 +0100

    correcciones

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 'use strict';
 var range = document.querySelector("#range");
 var valoracionbtn = document.querySelector("#valoracionbtn");
+var mostarcuenta = document.querySelector("#cuentabtn");
+var cuenta = document.querySelector("#cuenta");
+var dia = document.querySelector("#diabtn");
+var fecha1 = document.querySelector("#fecha");
 valoracionbtn.addEventListener("click", function () {
     alert("Has valorado con " + range.value + " puntos");
 });
-var mostarcuenta = document.querySelector("#cuentabtn");
-var cuenta = document.querySelector("#cuenta");
 mostarcuenta.addEventListener("click", function () {
     alert("le informamos que su numero de cuenta bancaria es: " + cuenta.value);
 });
-var dia = document.querySelector("#diabtn");
-var fecha1 = document.querySelector("#fecha");
 dia.addEventListener("click", function () {
     alert("este es la fecha de envio " + fecha1.value + " ");
 });
--- ts/index.ts
+++ ts/index.ts
@@ -2,24 +2,19 @@
 
 const range = (document.querySelector("#range") as HTMLFormElement)
 const valoracionbtn = (document.querySelector("#valoracionbtn") as HTMLFormElement)
+const mostarcuenta = (document.querySelector("#cuentabtn") as HTMLFormElement)
+const cuenta = (document.querySelector("#cuenta") as HTMLFormElement)
+const dia = (document.querySelector("#diabtn") as HTMLFormElement)
+const fecha1 = (document.querySelector("#fecha") as HTMLFormElement)
 
     valoracionbtn.addEventListener("click",()=>{
         alert(`Has valorado con ${range.value} puntos`)
     })
 
-
-const mostarcuenta = (document.querySelector("#cuentabtn") as HTMLFormElement)
-const cuenta = (document.querySelector("#cuenta") as HTMLFormElement)
-
-
     mostarcuenta.addEventListener("click",()=>{
             alert(`le informamos que su numero de cuenta bancaria es: ${cuenta.value}`)
-        })
-
-const dia = (document.querySelector("#diabtn") as HTMLFormElement)
-const fecha1 = (document.querySelector("#fecha") as HTMLFormElement)
+    })
 
     dia.addEventListener("click",()=>{
         alert(`este es la fecha de envio ${fecha1.value} `)
     })
-    
