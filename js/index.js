c0efed29dd652462910d177e38bcfd90e1c2795b
melvi30 <marianamujica30@gmail.com>
2021-02-15 11:22:23 +0100

    quitar void

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 var range = document.querySelector("#range");
 var valoracionbtn = document.querySelector("#valoracionbtn");
 valoracionbtn.addEventListener("click", function () {
-    alert("Has valorado con " + range + " puntos");
+    alert("Has valorado con " + range.value + " puntos");
 });
 var mostarcuenta = document.querySelector("#cuentabtn");
 var cuenta = document.querySelector("#cuenta");
 mostarcuenta.addEventListener("click", function () {
-    alert("le informamos que su n\u00FAmero de cuenta bancaria es: " + cuenta);
+    alert("le informamos que su n\u00FAmero de cuenta bancaria es: " + cuenta.value);
 });
 var dia = document.querySelector("#diabtn");
 var fecha1 = document.querySelector("#fecha");
--- ts/index.ts
+++ ts/index.ts
@@ -3,8 +3,8 @@
 const range = (document.querySelector("#range") as HTMLFormElement)
 const valoracionbtn = (document.querySelector("#valoracionbtn") as HTMLFormElement)
 
-    valoracionbtn.addEventListener("click",(): void =>{
-        alert(`Has valorado con ${ range } puntos`)
+    valoracionbtn.addEventListener("click",()=>{
+        alert(`Has valorado con ${range.value} puntos`)
     })
 
 
@@ -12,13 +12,13 @@ const mostarcuenta = (document.querySelector("#cuentabtn") as HTMLFormElement)
 const cuenta = (document.querySelector("#cuenta") as HTMLFormElement)
 
 
-    mostarcuenta.addEventListener("click",(): void =>{
-            alert(`le informamos que su número de cuenta bancaria es: ${cuenta}`)
+    mostarcuenta.addEventListener("click",()=>{
+            alert(`le informamos que su número de cuenta bancaria es: ${cuenta.value}`)
         })
 
 const dia = (document.querySelector("#diabtn") as HTMLFormElement)
 const fecha1 = (document.querySelector("#fecha") as HTMLFormElement)
 
-    dia.addEventListener("click",(): void => {
+    dia.addEventListener("click",()=>{
         alert(`este es el ${Date}`)
     })
